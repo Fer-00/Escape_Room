950a93536f77a82c967261480854bb5d5ed62ca9
Fer-00 <drica.jag@outlook.com>
2025-09-02 16:07:42 -0300

    Esqueleto - Tela Inicial

--- home.py
+++ home.py
@@ -6,7 +6,11 @@ class Home:
 	def __init__(self):
 		
 	def update(self):
-		pass
+		if px.btnp(px.KEY_Q):
+			px.quit()
+		elif px.btnp(px.KEY_SPACE) or px.btnp(px.KEY_RETURN):
+			frame.jogo_start() #variável pertencente a frames que será declarada futuramente
+		elif mouse_x == 
 
 	def draw(self):
 		pass
