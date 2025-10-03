f0bfbe3a29ad51726f99d932933505c83cf54f00
Fer-00 <drica.jag@outlook.com>
2025-10-02 22:40:08 -0300

    Contruindo método cria objeto

--- itens.py
+++ itens.py
@@ -1,12 +1,28 @@
-import json, os
+import json, os, pyxel
 
-class Itens():
-	def __init__(self):
-		try:
-			with open('itens.json','r') as file:
-				json_itens = json.load(file)
-				print(json_itens)
-		except ValueError as e:
-			print("Erro: ",str(e))
-#	def objeto(self):
-Itens()
+class Itens(object):
+	def __init__(self,**kwargs):
+		for key,valor in kwargs.items():
+			setattr(self,key,valor)
+	
+	def __repr__(self):
+		return f"<Item {self.__dict__}>"
+	
+	def __del__(self):
+		return True
+
+	def criaObjeto(self,nome,x,y):
+		if nome not in self.itens:
+			raise ValueError(f"Item {nome} não existe")
+		else:
+			pyxel.blt(x, y, 0,54,116,5,10,0) #checar esse comando
+
+
+try:
+	with open('itens.json','r') as file:
+		json_itens = json.load(file)
+		itens = [Itens(**json_itens[item]) for item in json_itens]
+		for item in itens:
+			print(item.nome,"-",item.comprimento,"-",item.altura)
+except ValueError as e:
+	print("Erro: ",str(e))
