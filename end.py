549db98b3e3021b9cd129eed132ad57981b1a075
Fer-00 <drica.jag@outlook.com>
2025-10-07 11:53:26 -0300

    Tela final - Ranking

--- end.py
+++ end.py
@@ -14,17 +14,18 @@ class End:
 	def tela_final(self, tempo_final = "Você Perdeu!!"):
 		px.cls(0)
 		px.text(40,10,"FIM DE JOGO",8)
-	#	px.text(30,20,f"Tempo atual:{tempo_final}s",7)
-	#	px.text(30,30,f"RANKING:",10)
-	#	for i,j in enumerate(self.top5[:5]):
-	#		px.text(60,20+i*10,f"{i+1}: {j} s",11)
-	#	px.text(30,40,f"Precione ESC para sair",6)
+		px.text(30,20,f"Tempo atual:{tempo_final}s",7)
+		px.text(30,30,f"RANKING:",10)
+		for i,j in enumerate(self.top5[:5]):
+			minutos = j//60
+			segundos = j % 60
+			px.text(60,40+i*10,f"{i+1}: {minutos}:{segundos} s",11)
 
-		px.text(30,20,"OBRIGADO POR JOGAR!",7)
-		px.text(40,40,"Feito por:",7)
-		px.text(30,50,"Adriane e Victtor",7)
+#		px.text(30,20,"OBRIGADO POR JOGAR!",7)
+		px.text(30,90,"Por: Adriane e Victtor",7)
 
-		px.text(20,100,"Precione ESC para sair",6)
+		px.text(20,110,"Precione ESC para sair",6)
+
 	def populaTop5(self):
 		if not os.path.exists(scores):
 			self.top5 = []
